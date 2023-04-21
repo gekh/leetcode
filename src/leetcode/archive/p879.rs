b643b565c9983b141a6a34c854c3f20f877433d2
German Khokhlov <lkg0dzre@gmail.com>
2023-04-21 15:57:48 +0530

    879. Profitable Schemes

--- src/leetcode/archive/p879.rs
+++ src/leetcode/archive/p879.rs
@@ -8,39 +8,27 @@ impl Solution {
     let mut dp = vec![vec![0;min_profit+1]; n+1];
     dp[0][0] = 1;
 
-    // println!("n:{n}");
-    // println!("min_profit:{min_profit}");
-
-
     for i in 0..group.len() {
-      // println!();
       let g = group[i] as usize;
       let p = profit[i] as usize;
-      // println!("p:{p}, g:{g}");
 
       if n < g {
         continue;
       }
 
       for nn in (0..=(n-g)).rev() {
-        // println!("{nn} {}", nn+g);
         for pp in 0..=min_profit {
           let minpp = min_profit.min(p+pp);
-          dp[nn+g][minpp] = (dp[nn+g][minpp] + dp[nn][pp]) % 1_000_007;
+          dp[nn+g][minpp] = (dp[nn+g][minpp] + dp[nn][pp]) % 1_000_000_007;
         }
       }
 
-      // println!();
-      // println!("dp:");
-      // for row in &dp {
-      //   println!("{row:?}");
-      // }
     }
 
     let mut out = 0;
 
     for i in 0..=n {
-      out = (out + dp[i][min_profit]) % 1_000_007;
+      out = (out + dp[i][min_profit]) % 1_000_000_007;
     }
 
     out
--- src/leetcode/archive/p879_test.rs
+++ src/leetcode/archive/p879_test.rs
@@ -1,10 +1,10 @@
 use super::p879::Solution;
 
 pub fn test() {
-    // assert_eq!(call(5,3,vec![2,2], vec![2,3]), 2);
-    // assert_eq!(call(10,5,vec![2,3,5], vec![6,7,8]), 7);
-    // assert_eq!(call(10,5,vec![2,3,5], vec![1,2,3]), 2);
-    // assert_eq!(call(5,5,vec![2,3,5], vec![6,7,8]), 4);
+    assert_eq!(call(5,3,vec![2,2], vec![2,3]), 2);
+    assert_eq!(call(10,5,vec![2,3,5], vec![6,7,8]), 7);
+    assert_eq!(call(10,5,vec![2,3,5], vec![1,2,3]), 2);
+    assert_eq!(call(5,5,vec![2,3,5], vec![6,7,8]), 4);
     assert_eq!(call(64,0,vec![80,40], vec![88,88]), 2);
 }
 
